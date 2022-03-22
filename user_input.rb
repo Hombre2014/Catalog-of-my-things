5529df42116b4f5206406a164f3b268574f97615
Charles Gobina <charles.gobina@gmail.com>
2022-03-22 20:35:17 +0100

    Update user_input.rb

--- user_input.rb
+++ user_input.rb
@@ -5,21 +5,6 @@ require_relative 'save_data'
 class UserInput
   include Helpers
 
-#   def compute_choice(choice)
-#     case choice
-#     when '1' then list_books
-#     when 2 then list_music_albums
-#     when 3 then list_games
-#     when 4 then list_genres
-#     when 5 then list_labels
-#     when 6 then list_authors
-#     when 7 then add_book
-#     when 8 then add_music_album
-#     when 9 then add_game
-#     when 10 then exit_app
-#     end
-#   end
-
   def initialize(choice, music_albums, genres)
     @choice = choice
     @music_albums = music_albums
