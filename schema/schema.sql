2df464aadd840a5165594c0971edb0d1537b5aaf
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-03-24 10:50:08 +0100

    Correct schema file

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,9 +1,22 @@
 CREATE DATABASE catalog_of_my_things;
 
+CREATE TABLE author (
+	id INT GENERATED ALWAYS AS IDENTITY,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	PRIMARY KEY(id)
+);
+
 CREATE TABLE genres (
 	id  INT GENERATED ALWAYS AS IDENTITY,
 	name VARCHAR(100),
 	PRIMARY KEY(id)
+);
+
+CREATE TABLE labels (
+	id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	title VARCHAR(100),
+	color VARCHAR(10)
 );
 
 CREATE TABLE item (
@@ -16,39 +29,26 @@ CREATE TABLE item (
 	PRIMARY KEY(id),
 	FOREIGN KEY (genre_id) REFERENCES genres (id),
 	FOREIGN KEY (author_id) REFERENCES author (id),
-	FOREIGN KEY (label_id) REFERENCES labels (id),
-);
-
-CREATE TABLE music_albums (
-	id  INT,
-	name VARCHAR(100),
-	on_spotify BOOLEAN,
-	FOREIGN KEY(id) REFERENCES item(id)
-);
-
-CREATE TABLE games (
-	id INT GENERATED ALWAYS AS IDENTITY,
-	multiplayer BOOLEAN,
-	last_played_at DATE,
-	PRIMARY KEY(id)
-);
-
-CREATE TABLE author (
-	id INT GENERATED ALWAYS AS IDENTITY,
-	first_name VARCHAR(50),
-	last_name VARCHAR(50),
-	PRIMARY KEY(id)
+	FOREIGN KEY (label_id) REFERENCES labels (id)
 );
 
 create table book(
 	id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 	publisher VARCHAR(100),
 	cover_state VARCHAR(10),
-	FOREIGN KEY (id) REFERENCES item (id),
-)
+	FOREIGN KEY (id) REFERENCES item (id)
+);
 
-CREATE TABLE labels (
+CREATE TABLE music_albums (
+	id  INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	name VARCHAR(100),
+	on_spotify BOOLEAN,
+	FOREIGN KEY(id) REFERENCES item(id)
+);
+
+CREATE TABLE games (
 	id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-	title VARCHAR(100),
-	color VARCHAR(10),
+	multiplayer BOOLEAN,
+	last_played_at DATE,
+  FOREIGN KEY (id) REFERENCES item (id)
 );
